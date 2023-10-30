1879d369ad4141d97fab594ff108c6c53b2581d2
nishanknavelkar <nishanknavelkar1997@gmail.com>
2023-10-30 21:46:14 +0100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nishank', // Usually your GitHub org/user name.
-  projectName: 'nishanknavelkar.github.io', // Usually your repo name.
+  projectName: 'blog', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
